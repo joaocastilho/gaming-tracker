544c35c6df2d0f39efb73af20fcf7f4ec08d6c99
joaocastilho <joaocastilho@users.noreply.github.com>
2025-11-07 11:25:50 +0000

    deploy: aec079fcb84ee0365cb06147a4707ae77b1225e1

--- _app/immutable/chunks/Cg38D0CO.js
+++
@@ -1,1 +0,0 @@
-import{T as e,a as r}from"./CVtoJdcG.js";function o(t){const a=r[t]||t;return e[a]||"bg-gray-600 text-white"}export{o as g};
--- _app/immutable/nodes/0.Cqs7pi8L.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/Dd4m1J6I.js";export{e as component,n as universal};
