94b604dc0b0cb571f4da59be4b0636eca5f975ea
João Castilho <joao@example.com>
2025-11-20 13:39:39 +0000

    build(postcss): update browserslist for Safari >=15.1 and generalize webkit margin removal

--- postcss.config.js
+++ postcss.config.js
@@ -5,14 +5,14 @@ export default {
 	plugins: [
 		tailwindcss(),
 		autoprefixer({
-			overrideBrowserslist: ['> 1%', 'last 2 versions', 'Firefox ESR', 'not dead', 'Safari >= 9'],
+			overrideBrowserslist: ['> 1%', 'last 2 versions', 'Firefox ESR', 'not dead', 'Safari >= 15.1'],
 			grid: true,
 			flexbox: 'no-2009'
 		}),
 		{
 			postcssPlugin: 'remove-legacy-webkit-margins',
 			Declaration(decl) {
-				if (decl.prop === '-webkit-margin-after' || decl.prop === '-webkit-margin-before') {
+				if (decl.prop.startsWith('-webkit-margin-')) {
 					decl.remove();
 				}
 			}
