b67c59274b2ae8c38bc51c322d963b708da647fa
Joao Castilho <joaocastilho.work@gmail.com>
2026-04-11 14:02:40 +0100

    fix: remove autoprefixer from postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,8 @@
 import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
 
 export default {
 	plugins: [
 		tailwindcss(),
-		autoprefixer({
-			overrideBrowserslist: ['> 1%', 'last 2 versions', 'Firefox ESR', 'not dead', 'Safari >= 15.1'],
-			grid: true,
-			flexbox: 'no-2009',
-		}),
 		{
 			postcssPlugin: 'remove-legacy-webkit-margins',
 			Declaration(decl) {
